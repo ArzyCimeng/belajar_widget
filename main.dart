99d5547aca09dc243766666f702ff451f3d6e425
ArzyCimeng <zyy.febrian8@gmail.com>
2024-11-12 11:29:30 +0700

    versi 2 container

--- main.dart
+++ main.dart
@@ -1,64 +1,67 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Root widget
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Login Page'),
+          title: Text('Container Widget Example'),
         ),
         body: Center(
-          child: Builder(
-            builder: (context) {
-              return Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 100),
-                child: Column(
-                  children: [
-                    Image.asset('logo.png'),
-                    const SizedBox(height: 20),
-                    TextField(
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Email',
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    TextField(
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Password',
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    const Text(
-                      'Forgot Password ?',
-                      style: TextStyle(color: Colors.lightBlue),
-                    ),
-                    const SizedBox(height: 8.0),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.lightBlueAccent),
-                      onPressed: () {
-                        print('Click!');
-                      },
-                      child: const Text('login'),
-                    ),
-                    const SizedBox(height: 20),
-                    const Text('New User? Get Account'),
-                    const SizedBox(height: 20),
-                  ],
+          child: Container(
+            height: 230,
+            width: double.infinity,
+            margin: const EdgeInsets.all(20.0),
+            padding: const EdgeInsets.all(15.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              gradient: const LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 3, 255, 234),
+                  Colors.blue,
+                  Color.fromARGB(255, 0, 234, 255),
+                  Color.fromARGB(255, 123, 0, 255)
+                ],
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Judul: Benda Flutter',
+                  style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black),
                 ),
-              );
-            },
+                const SizedBox(height: 12),
+                const Divider(color: Colors.black),
+                const Text(
+                  'Deskripsi: Ini adalah tata letak seperti kartu yang menggunakan widget Kontainer. Ini memiliki padding, margin, dan bayangan kotak.',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 17,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                SizedBox(
+                    height: 40,
+                    width: double.infinity,
+                    child: ElevatedButton(
+                        onPressed: () {},
+                        child: const Text(
+                          'kunjungi untuk membaca lebih lanjut',
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 0, 0, 0),
+                              fontWeight: FontWeight.bold),
+                        )))
+              ],
+            ),
           ),
         ),
       ),
